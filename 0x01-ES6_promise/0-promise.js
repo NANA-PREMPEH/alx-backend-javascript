79626d4d1515079323eb2f83bbc2b193e2699392
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2023-01-25 01:26:31 +0000

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,20 @@
-const getResponseFromAPI = () => new Promise((resolve, reject) => {
+/**
+ * getResponseFromAPI is a function that returns a promise,
+ * this function can be used to make an API call and handle the response
+ */
+function getResponseFromAPI() {
+  // Returns a new promise
+  return new Promise((resolve, reject) => {
+    // here you can make your API call and check the response
+    // if the response is successful, then call the resolve function
     if (true) {
         resolve();
-    } else {
+    } 
+    // if the response is unsuccessful, call the reject function
+    else {
         reject();
     }
-});
+  });
+}
 
 export default getResponseFromAPI;
