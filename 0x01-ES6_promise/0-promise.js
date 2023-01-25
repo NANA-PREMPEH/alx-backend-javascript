70ceadc858878aed5f48c694db1a0411a07aea6a
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2023-01-25 01:27:51 +0000

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,19 +1,12 @@
-/**
- * getResponseFromAPI is a function that returns a promise,
- * this function can be used to make an API call and handle the response
- */
 function getResponseFromAPI() {
-  // Returns a new promise
   return new Promise((resolve, reject) => {
-    // here you can make your API call and check the response
-    // if the response is successful, then call the resolve function
-    if (true) {
+    /* eslint-disable */
+      if (true) {
         resolve();
-    } 
-    // if the response is unsuccessful, call the reject function
-    else {
+      } else {
         reject();
-    }
+      }
+      /* eslint-enable */
   });
 }
 
