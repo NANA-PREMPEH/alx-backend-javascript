45f3ac1785a59f4f8f22e6dd25e0f6dfabb0c202
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2023-01-25 01:18:04 +0000

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,24 +1,10 @@
 function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
-        // Make API call here
-        fetch('https://some-api.com/data')
-            .then(response => {
-                if (response.ok) {
-                    resolve(response.json());
-                } else {
-                    reject(Error(response.statusText));
-                }
-            })
-            .catch(error => {
-                reject(error);
-            });
+        // make API call
+        if (response from API is successful) {
+            resolve(response);
+        } else {
+            reject(error);
+        }
     });
 }
-
-getResponseFromAPI()
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log(error);
-    });
