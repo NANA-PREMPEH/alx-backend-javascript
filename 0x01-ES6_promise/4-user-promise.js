41250f0aff453f04bdb709c2cac801be739f79b2
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2023-01-25 03:23:43 +0000

    Update 4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,10 @@
 function signUpUser(firstName, lastName) {
-    return new Promise(resolve => {
-        resolve({
-            firstName: firstName,
-            lastName: lastName
-        });
-    });
+  return Promise.resolve(
+    {
+      firstName,
+      lastName,
+    },
+  );
 }
+
+export default signUpUser;
