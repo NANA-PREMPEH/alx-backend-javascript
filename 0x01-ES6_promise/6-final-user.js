b227bae6d3575a4e9ba6d2459782eae503f155a8
William Osei Prempeh <52207181+NANA-PREMPEH@users.noreply.github.com>
2023-01-25 01:55:54 +0000

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,28 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export const handleProfileSignup = (firstName, lastName, fileName) => {
-  // create an array to hold the promise statuses and values/errors
-  const results = [];
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfilled',
+    value: data,
+  }));
 
-  // call the signUpUser function and pass in the firstName and lastName as arguments
-  const signUpPromise = signUpUser(firstName, lastName)
-    .then((value) => {
-      results.push({ status: 'resolved', value });
-    })
-    .catch((error) => {
-      results.push({ status: 'rejected', error });
-    });
+  const fileData = await uploadPhoto(fileName).catch((err) => ({
+    status: 'rejected',
+    value: err.toString(),
+  }));
 
-  // call the uploadPhoto function and pass in the fileName as an argument
-  const uploadPromise = uploadPhoto(fileName)
-    .then((value) => {
-      results.push({ status: 'resolved', value });
-    })
-    .catch((error) => {
-      results.push({ status: 'rejected', error });
-    });
-
-  // wait for both promises to settle
-  return Promise.all([signUpPromise, uploadPromise]).then(() => results);
-};
+  return Promise.resolve([userData, fileData]);
+}
